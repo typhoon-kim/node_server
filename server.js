cef6891cb1cbfc9ab5e35fd55ba5e9f6c0e1db8c
typhoon-kim <82020073+typhoon-kim@users.noreply.github.com>
2024-04-15 14:00:41 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end('Hello World');
+    
+});
+
+server.listen(8080);
